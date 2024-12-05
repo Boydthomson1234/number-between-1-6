1f86415b275125bcad04b1573396bba791ccbaa8
Boydthomson1234 <boyd.thomson@stu.ocsb.ca>
2024-12-05 11:01:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from random import *
 ranumb=randint(1, 6)
 #user inputs a number from 1-6
 usernumb=int(input("Guess a number from 1-6: "))
+#if the nubmber is the same as the computer's number, the user wins
 if ranumb==usernumb:
   print("You win!")
+#if the number is different then the computers number, the user loses
 if ranumb!=usernumb:
   print("you lose")  
